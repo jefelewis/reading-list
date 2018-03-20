ebf5b02696dbcdbb67b88f0b89f09ab006f12ec5
jefelewis <jeffylew77@gmail.com>
2018-03-19 18:07:32 -0700

    Router skeleton working

---
+++ server/router.js
@@ -0,0 +1,40 @@
+// Requirements: Express Router
+const router = require('express').Router();
+
+
+// Imports: Database Controllers
+import sqlController from './controllers/sqlcontroller.js';
+import mongodbController from './controllers/mongodbcontroller.js';
+
+// Imports: API Controllers
+import apiController from './controllers/apicontroller.js';
+
+
+
+
+/****************************** ROUTES: Homepage ******************************/
+
+
+/****************************** ROUTES: Homepage ******************************/
+
+
+
+
+/*************************** ROUTES: SQL Controller ***************************/
+
+
+/*************************** ROUTES: SQL Controller ***************************/
+
+
+
+
+
+/************************* ROUTES: MongoDB Controller *************************/
+
+
+/************************* ROUTES: MongoDB Controller *************************/
+
+
+
+// Exports
+export default router;
