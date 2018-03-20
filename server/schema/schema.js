549bdc800341d5412e4a783762f83357891e931e
jefelewis <jeffylew77@gmail.com>
2018-03-20 00:03:22 -0700

    Schema file skeleton complete

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,3 +1,38 @@
 // Imports: GraphQL
 const graphql = require('graphql');
 
+// Imports: GraphQL Packages
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+
+// GraphQL: Schema
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    genre: {type: GraphQLString}
+  })
+});
+
+
+// GraphQL: Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: {id: {type: GraphQLString}},
+      resolve(parent, args){
+        //
+        
+      }
+    }
+  }
+});
+
+
+// Exports
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
