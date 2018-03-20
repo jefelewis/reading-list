6bf7d83a0ee225ae631c8682a2afebfdb7830e6d
jefelewis <jeffylew77@gmail.com>
2018-03-19 22:58:56 -0700

    Fixing an ES6 import/export compatiblity issue

---
+++ client/index.js
@@ -0,0 +1,10 @@
+// // Imports: React
+// import react from 'react';
+// import reactDOM from 'react-dom';
+
+
+// // Render App
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('app')
+// );
