1e6de87aef90e4f9a04f7892732cda046a6759a8
jefelewis <jeffylew77@gmail.com>
2018-04-02 13:20:15 -0700

    Adding addAuthor mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -14,50 +14,37 @@ const {
 // Imports: Lodash
 const _ = require('lodash');
 
-// GraphQL: Dummy Data
-let books = [
-  {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
-  {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
-  {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
-  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
-  {name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
-  {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'},
-];
-
-
-let authors = [
-  {name: 'Patrick Rothfuss', age: '52', id: '1'},
-  {name: 'Brandon Sanderson', age: '42', id: '2'},
-  {name: 'Terry Pratchett', age: '66', id: '3'}
-];
+// Imports: MongoDB Models
+const BOOK = require('../models/book.js');
+const AUTHOR = require('../models/author.js');
 
 
 // GraphQL: Schema
-const BookType = new GraphQLObjectType({
+const BOOKTYPE = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: {type: GraphQLID},
-    name: {type: GraphQLString},
-    genre: {type: GraphQLString},
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
     author: {
-      type: AuthorType,
+      type: AUTHORTYPE,
       resolve(parent, args) {
-        return _.find(authors, {id: parent.authorId})
+        // return _.find(authors, {id: parent.authorId})
       }
     }
   })
 });
 
-const AuthorType = new GraphQLObjectType({
+const AUTHORTYPE = new GraphQLObjectType({
   name: 'Author',
   fields: () => ({
-    id: {type: GraphQLID},
-    name: {type: GraphQLString},
-    age: {type: GraphQLInt},
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
     books: {
-      type: new GraphQLList(BookType),
+      type: new GraphQLList(BOOKTYPE),
       resolve(parent, args) {
-        return _.filter(books, {authorId: parent.id})
+        // return _.filter(books, {authorId: parent.id})
       }
     }
   })
@@ -65,42 +52,69 @@ const AuthorType = new GraphQLObjectType({
 
 
 // GraphQL: Root Query
-const RootQuery = new GraphQLObjectType({
+const ROOTQUERY = new GraphQLObjectType({
   name: 'RootQueryType',
-  fields: {
+  fields: () => ({
     book: {
-      type: BookType,
-      args: {id: {type: GraphQLID}},
+      type: BOOKTYPE,
+      args: { id: { type: GraphQLID }},
       resolve(parent, args) {
         // Grab data from Database/API
-        return _.find(books, {id: args.id});
+        // return _.find(books, {id: args.id});
       }
     },
     author: {
-      type: AuthorType,
-      args: {id: {type: GraphQLID}},
+      type: AUTHORTYPE,
+      args: { id: { type: GraphQLID }},
       resolve(parent, args) {
         // Grab data from Database/API
-        return _.find(authors, {id: args.id});
+        // return _.find(authors, {id: args.id});
       }
     },
     books: {
-      type: new GraphQLList(BookType),
+      type: new GraphQLList(BOOKTYPE),
       resolve(parent, args) {
-        return books;
+        // return books;
       }
     },
     authors: {
-      type: new GraphQLList(AuthorType),
+      type: new GraphQLList(AUTHORTYPE),
       resolve(parent, args) {
-        return authors;
+        // return authors;
       }
     }
-  }
+  })
+});
+
+
+// GraphQL: Mutations
+const MUTATION = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: () => ({
+    // Add Author
+    addAuthor: {
+      type: AUTHORTYPE,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        // Create new instance
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        });
+
+        // Save new instance
+        return author.save();
+      }
+    }
+  })
 });
 
 
 // Exports
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: ROOTQUERY,
+  mutation: MUTATION
 });
