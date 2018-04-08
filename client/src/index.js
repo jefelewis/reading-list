813579e4652063c42805c2863e13f96e845eae78
jefelewis <jeffylew77@gmail.com>
2018-04-07 23:46:53 -0700

    Importing CSS + components to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,12 @@
 const react = require('react');
 const reactDOM = require('react-dom');
 
+// Imports: CSS
+import './index.css';
+
+// Imports: Components
+import App from './App.js';
+
 
 // Render App
 ReactDOM.render(
