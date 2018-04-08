bcd0def1f89a0bb8de081647c472b3722d06ab61
jefelewis <jeffylew77@gmail.com>
2018-04-07 23:49:53 -0700

    Working on frontend skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
         <div id="main">
-          <h1>Ninja's Reading List</h1>
+          <h1>Reading List</h1>
       </div>
     );
   }
