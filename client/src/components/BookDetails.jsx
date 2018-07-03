91bcdd2416b4f2d17a636fa4ccdec6e6b94e2106
Jeff Lewis <jeffylew77@gmail.com>
2018-07-03 10:47:33 -0700

    [Dev] Adding BookDetails component

---
+++ client/src/components/BookDetails.jsx
@@ -0,0 +1,8 @@
+// Imports: React
+import React, { Component } from 'react';
+
+// Imports: Apollo
+import { graphql } from 'react-apollo';
+
+// Imports: getAuthors Query
+import { getBooksQuery } from '../queries/queries.js';
