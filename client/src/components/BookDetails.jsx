d8ff9b9bf2a65acf47f042d0f86bcaddec4a8767
Jeff Lewis <jeffylew77@gmail.com>
2018-07-03 20:05:04 -0700

    [Dev] Adding displayBookDetails function to BookDetails component

--- client/src/components/BookDetails.jsx
+++ client/src/components/BookDetails.jsx
@@ -11,17 +11,51 @@ import { getBookQuery } from '../queries/queries.js';
 // Component: BookDetails
 class BookDetails extends Component {
 
-    // Render
-    render() {
-      return (
-        <div id="book-details">
-            <p>Output Book Details Here</p>
+  // Function: Display details from a given book
+  displayBookDetails() {
+    const { book } = this.props.data;
+
+    // If Book exists
+    if(book) {
+      return(
+        <div>
+          <h2>{ book.name }</h2>
+          <p>{ book.genre }</p>
+          <p>{ book.author.name }</p>
+          <p>All books by { book.author.name }</p>
+          <ul className="other-books">
+            { book.author.books.map((book) => {
+              return <li key={ book.id }>{ book.name }</li>
+            })}
+          </ul>
         </div>
-      );
+      )
+    }
+    // If book doesn't exist
+    else {
+      <div>No book selected</div>
     }
   }
+
+  // Render
+  render() {
+    return (
+      <div id="book-details">
+        {this.displayBookDetails()}
+      </div>
+    )
+  }
+}
 
 
 // Exports (Binding the getBooks query with the BookList component)
 // graphql(Query)(Component)
-export default graphql(getBookQuery)(BookDetails);
+export default graphql(
+  getBookQuery,
+  {options: (props) => {
+  return {
+    variables: {
+      id: props.bookId
+    }
+  }
+}})(BookDetails);
