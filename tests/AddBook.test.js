c48adb4e295d8aa63436393f625181fa0d25ecd4
Jeff Lewis <jeffylew77@gmail.com>
2018-07-06 09:24:09 -0700

    [Dev] Adding testing files for Jest and Enzyme

---
+++ tests/AddBook.test.js
@@ -0,0 +1,14 @@
+// Imports: React
+import React, { Component } from 'react';
+import { shallow } from 'enzyme';
+
+describe('Blah Blah Blah', () => {
+    it('does this', () => {
+        // Delcare variabes?
+
+
+        expect().toEqual()
+        //
+
+    })
+})
