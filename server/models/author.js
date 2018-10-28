a63fe956c5ba36c47a7ee9122e0e0f54cd9ddd16
Jeff Lewis <jeffylew77@gmail.com>
2018-10-28 00:21:48 -0700

    [Dev] Updating to ES6 + Adding Mongoose schema options

---
+++ server/models/author.js
@@ -0,0 +1,14 @@
+// Imports: Dependencies
+import mongoose from 'mongoose';
+
+// Mongoose: Author Schema
+const AUTHOR = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  age: { type: Number, required: true, trim: true },
+},
+{
+  timestamps: true,
+});
+
+// Exports
+export default mongoose.model('Author', AUTHOR);
---
+++ server/models/book.js
@@ -0,0 +1,15 @@
+// Imports: Dependencies
+import mongoose from 'mongoose';
+
+// Mongoose: Book Schema
+const BOOK = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  genre: { type: String, required: true, trim: true },
+  authorId: { type: String, required: true, trim: true },
+},
+{
+  timestamps: true,
+});
+
+// Exports
+export default mongoose.model('Book', BOOK);
